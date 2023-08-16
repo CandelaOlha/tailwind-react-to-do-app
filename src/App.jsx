de0e3db8f7c94b91cb0babd5597bb7a306b3ec4d
“c.b.olha” <c.b.olha@viseven.com>
2023-08-16 16:41:06 -0300

    added icons and some details

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,36 @@
+import MoonIcon from "./components/icons/MoonIcon";
+import CrossIcon from "./components/icons/CrossIcon";
+
 const App = () => {
   return (
     <div>
       <header className="bg-[url('./assets/bg-mobile-light.jpg')] p-10">
-        <div className="flex justify-between mb-5">
+        <div className="flex justify-between mb-8">
           <h1 className="uppercase text-xl text-white font-bold tracking-widest">
             To do App
           </h1>
-          <button className="text-white">☽</button>
+          <button className="text-white">
+            <MoonIcon />
+          </button>
         </div>
         <form>
           <input
             type="text"
             className="container p-3 rounded-md text-xs text-gray-400 font-semibold"
-            placeholder="Create new task"
+            placeholder="Create a new task..."
           />
         </form>
       </header>
       <main className="p-10">
         <div className="bg-white shadow rounded">
           <article className="flex justify-around py-3 border-b border-gray-500 border-opacity-20 border-solid">
-            <button className="text-gray-700">✓</button>
-            <p className="text-sm text-gray-700 font-medium">
+            <button className="h-5 w-5 border border-gray-500 border-solid rounded-full"></button>
+            <p className="text-sm text-gray-500 font-medium">
               This is a task description
             </p>
-            <button>X</button>
+            <button>
+              <CrossIcon />
+            </button>
           </article>
         </div>
       </main>
