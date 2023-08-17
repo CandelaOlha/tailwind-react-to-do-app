85ac93ebb163214c2e13774f4765fa9aa0097f33
“c.b.olha” <c.b.olha@viseven.com>
2023-08-17 16:36:26 -0300

    fixed some styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import CrossIcon from "./components/icons/CrossIcon";
 
 const App = () => {
   return (
-    <div>
+    <div className="bg-gray-100 h-screen">
       <header className="bg-[url('./assets/bg-mobile-light.jpg')] p-10">
         <div className="flex justify-between mb-8">
           <h1 className="uppercase text-xl text-white font-bold tracking-widest">
@@ -16,14 +16,14 @@ const App = () => {
         <form>
           <input
             type="text"
-            className="container p-3 rounded-md text-xs text-gray-400 font-semibold"
+            className="container py-3 px-5 rounded-md text-xs text-gray-400 font-semibold"
             placeholder="Create a new task..."
           />
         </form>
       </header>
       <main className="p-10">
-        <div className="bg-white shadow rounded">
-          <article className="flex justify-around py-3 border-b border-gray-500 border-opacity-20 border-solid">
+        <div className="bg-white shadow rounded-t">
+          <article className="flex justify-between p-5 border-b border-gray-500 border-opacity-20 border-solid">
             <button className="h-5 w-5 border border-gray-500 border-solid rounded-full"></button>
             <p className="text-sm text-gray-500 font-medium">
               This is a task description
@@ -33,14 +33,20 @@ const App = () => {
             </button>
           </article>
         </div>
+        <section className="bg-white rounded-b flex justify-between p-5">
+          <p className="text-xs text-gray-400 font-semibold">5 items left</p>
+          <button className="text-xs text-gray-400 font-semibold">
+            Clear Completed
+          </button>
+        </section>
       </main>
-      <footer className="flex justify-around p-10">
-        <button className="text-xs text-gray-400 font-semibold">All</button>
-        <button className="text-xs text-gray-400 font-semibold">Active</button>
-        <button className="text-xs text-gray-400 font-semibold">
+      <section className="flex justify-around bg-white rounded-md p-5 m-10">
+        <button className="text-sm text-gray-400 font-semibold">All</button>
+        <button className="text-sm text-gray-400 font-semibold">Active</button>
+        <button className="text-sm text-gray-400 font-semibold">
           Completed
         </button>
-      </footer>
+      </section>
     </div>
   );
 };
