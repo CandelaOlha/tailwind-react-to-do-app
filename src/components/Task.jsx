86da290518d8272b289617439b6b18f703a13ba9
“c.b.olha” <c.b.olha@viseven.com>
2023-08-23 12:16:43 -0300

    added check icon

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,10 +1,13 @@
 import CrossIcon from "./icons/CrossIcon";
+import CheckIcon from "./icons/CheckIcon";
 import PropTypes from "prop-types";
 
 const Task = ({ task }) => {
   return (
     <article className="flex justify-between p-5 border-b border-gray-500 border-opacity-20 border-solid">
-      <button className="h-5 w-5 border border-gray-500 border-solid rounded-full"></button>
+      <button className="flex justify-center items-center h-5 w-5 border border-gray-500 border-solid rounded-full">
+        <CheckIcon />
+      </button>
       <p className="text-sm text-gray-500 font-medium">{task.description}</p>
       <button>
         <CrossIcon />
